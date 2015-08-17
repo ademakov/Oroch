c85fa97545003d0c0f2a3af73275c8778de8a0ee
Aleksey Demakov <ademakov@gmail.com>
2015-08-18 05:39:35 +0600

    Fix test name.

---
+++ oroch/normal.h
@@ -0,0 +1,88 @@
+// normal.h
+//
+// Copyright (c) 2015  Aleksey Demakov
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#ifndef OROCH_NORMAL_H_
+#define OROCH_NORMAL_H_
+
+namespace oroch {
+
+template<typename T>
+class normal_codec
+{
+public:
+	using original_t = T;
+
+	static constexpr size_t
+	space(size_t nvalues)
+	{
+		return nvalues * sizeof(original_t);
+	}
+
+	template<typename DstIter, typename SrcIter>
+	static bool
+	encode(DstIter &dbegin, DstIter dend, SrcIter &sbegin, SrcIter send)
+	{
+		bool rc = true;
+		DstIter dst = dbegin;
+		SrcIter src = sbegin;
+
+		while (src < send) {
+			if (dst + sizeof(original_t) > dend) {
+				rc = false;
+				break;
+			}
+			*reinterpret_cast<original_t *>(dst) = *src++;
+			dst += sizeof(original_t);
+		}
+
+		dbegin = dst;
+		sbegin = src;
+		return rc;
+	}
+
+	template<typename DstIter, typename SrcIter>
+	static bool
+	decode(DstIter &dbegin, DstIter dend, SrcIter &sbegin, SrcIter send)
+	{
+		bool rc = true;
+		DstIter dst = dbegin;
+		SrcIter src = sbegin;
+
+		while (dst < dend) {
+			if (src + sizeof(original_t) > send) {
+				rc = false;
+				break;
+			}
+			*dst++ = *reinterpret_cast<original_t *>(src);
+			src += sizeof(original_t);
+		}
+
+		dbegin = dst;
+		sbegin = src;
+		return rc;
+	}
+};
+
+} // namespace oroch
+
+#endif /* OROCH_NORMAL_H_ */
---
+++ tests/unit/normal.cc
@@ -0,0 +1,29 @@
+#include "catch.hpp"
+
+#include <array>
+#include <oroch/normal.h>
+
+#define INTS 128
+
+TEST_CASE("normal value codec", "[normal]") {
+	using codec = oroch::normal_codec<int32_t>;
+	std::array<uint8_t, codec::space(INTS)> bytes;
+	std::array<uint32_t, INTS> integers;
+	std::array<uint32_t, INTS> integers2;
+
+	for (int i = 0; i < INTS; i++) {
+		integers[i] = i - INTS / 2;
+	}
+
+	auto b_it = bytes.begin();
+	auto i_it = integers.begin();
+	REQUIRE(codec::encode(b_it, bytes.end(), i_it, integers.end()));
+
+	b_it = bytes.begin();
+	i_it = integers2.begin();
+	REQUIRE(codec::decode(i_it, integers2.end(), b_it, bytes.end()));
+
+	for (int i = 0; i < INTS; i++) {
+		REQUIRE(integers2[i] == integers[i]);
+	}
+}
