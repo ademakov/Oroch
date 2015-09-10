7022e8ba8bc50184064955394b44bdff14f7d0c4
Aleksey Demakov <ademakov@gmail.com>
2015-09-10 21:11:30 +0600

    Add origin codec

---
+++ oroch/origin.h
@@ -0,0 +1,62 @@
+// origin.h
+//
+// Copyright (c) 2015  Aleksey Demakov
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#ifndef OROCH_ORIGIN_H_
+#define OROCH_ORIGIN_H_
+
+#include <oroch/integer_traits.h>
+
+namespace oroch {
+
+//
+// Encoding of a value as a difference relative to a given origin
+// (frame of reference).
+//
+template<typename T>
+class origin_codec
+{
+public:
+	using original_t = T;
+	using unsigned_t = typename integer_traits<original_t>::unsigned_t;
+
+	origin_codec(original_t origin) : origin_{origin} {}
+
+	unsigned_t
+	value_encode(original_t v) const
+	{
+		return unsigned_t(v - origin_);
+	}
+
+	original_t
+	value_decode(unsigned_t v) const
+	{
+		return original_t(v + origin_);
+	}
+
+private:
+	const original_t origin_;
+};
+
+} // namespace oroch
+
+#endif /* OROCH_ORIGIN_H_ */
